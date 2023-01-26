16ab1d741fa3f7cb0c229e03d5d7ebefcab120f4
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-01-26 19:24:09 -0300

    Metodo logarConta

--- backend/src/main/java/br/com/backend/interfaces/OperacaoConta.java
+++ backend/src/main/java/br/com/backend/interfaces/OperacaoConta.java
@@ -2,6 +2,6 @@ package br.com.backend.interfaces;
 
 import br.com.backend.modelo.entidades.Conta;
 
-public interface InterfaceConta {
+public interface OperacaoConta {
 	Conta logarConta(String cpf, String senhaCliente);
 }
--- backend/src/main/java/br/com/backend/modelo/servicos/ServicoConta.java
+++ backend/src/main/java/br/com/backend/modelo/servicos/ServicoConta.java
@@ -3,12 +3,12 @@ package br.com.backend.modelo.servicos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.backend.interfaces.InterfaceConta;
+import br.com.backend.interfaces.OperacaoConta;
 import br.com.backend.modelo.entidades.Conta;
 import br.com.backend.modelo.repositorios.RepositorioConta;
 
 @Service
-public class ServicoConta implements InterfaceConta {
+public class ServicoConta implements OperacaoConta {
 	@Autowired
 	private RepositorioConta repositorioConta;
 
